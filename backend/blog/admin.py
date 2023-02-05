8465fff8104af6c14e4582a5090de9d33c7979e4
Zamoca <suntail2002@naver.com>
2023-02-05 20:27:23 +0900

    build: start django backend app blog

---
+++ backend/blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from blog.models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'modify_dt')
+
+# Register your models here.
+
+
