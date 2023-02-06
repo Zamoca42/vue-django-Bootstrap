2012b7c1e7dd8d412e36c4b3266f2ec6d19869be
Zamoca <suntail2002@naver.com>
2023-02-06 21:25:54 +0900

    Fix: Correct typo in build process

--- backend/mysite/settings/base.py
+++ backend/mysite/settings/base.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS = [
 # STATIC_ROOT = 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIF / 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- backend/mysite/urls.py
+++ backend/mysite/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from mysite.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', views.HomeView.as_view(), name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('blog/', include('blog.urls')),
 ]
