80d559649836136fa04e7598cfd476916799853f
Zamoca <suntail2002@naver.com>
2023-02-16 19:28:39 +0900

    refactor: Send post detail directly from JavaScript in blog app

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import DetailView, TemplateView
 
-from blog.models import Post
+from blog.models import Post, Category
+from api.utils import obj_to_post, prev_next_post
 
+import json
 # Create your views here.
 
-class PostLV(ListView):
-    model = Post
-    # template_name = 'blog/post_list.html'
+# class PostLV(ListView):
+#     model = Post
+#     # template_name = 'blog/post_list.html'
+
+# class PostDV(TemplateView):
+#     template_name = 'blog/post_detail.html'
 
 class PostDV(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+
+        obj = context['object']
+        post = obj_to_post(obj)
+        prevPost, nextPost = prev_next_post(obj)
+
+        dataDict = {
+            'post': post,
+            'prevPost': prevPost,
+            'nextPost': nextPost,
+        }
+
+        context['myJson'] = json.dumps(dataDict)
+        return context
